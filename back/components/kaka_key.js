af9d2880afe2f1adb50956306a378346d287b89a
smilejakdu <ash982416@gmail.com>
2021-04-06 01:07:14 +0900

    kaka_key

---
+++ back/components/kaka_key.js
@@ -0,0 +1,3 @@
+module.exports = {
+  clientID: "02a7eec164d42a2aa15d0d29f2d4e7b3",
+};
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -2,18 +2,19 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const KakaoStrategy = require("passport-kakao").Strategy;
+const KakaoId= require("../components/kaka_key");
 
 passport.use(
   "kakao",
   new KakaoStrategy(
     {
-      clientID: "02a7eec164d42a2aa15d0d29f2d4e7b3",
+      clientID: KakaoId.clientID,
       callbackURL: "/auth/kakao/callback", // 위에서 설정한 Redirect URI
     },
     async (accessToken, refreshToken, profile, done) => {
       //console.log(profile);
       console.log(accessToken);
-      console.log("refreshToken : ",refreshToken);
+      console.log("refreshToken : ", refreshToken);
     }
   )
 );
