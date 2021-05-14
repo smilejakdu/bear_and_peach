a08afad2ab69189e46289dd16e239aebddc69d5a
smilejakdu <ash982416@gmail.com>
2021-05-14 20:04:35 +0900

    docker_script_compose

--- back/scripts/machine.sh
+++ back/scripts/machine.sh
@@ -5,4 +5,4 @@ ssh-add -k /Users/dujak/aws_key_pair/bear_and_peach.pem
 
 docker-machine create --driver generic --generic-ip-address=3.35.131.149 --generic-ssh-user ubuntu bear-and-peach-machine
 
-# 에러가 발생한다 . npm run deploy ==> error 확인 후 , 
+# 에러가 발생한다 . npm run deploy ==> error 확인 후  image , machine , aws 확인할것 
